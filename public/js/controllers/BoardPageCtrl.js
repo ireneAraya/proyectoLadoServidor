20127e3e2ff46cd8d081ca6fb2fb68215169936e
nalpizar <natalpizarm@gmail.com>
2016-11-27 22:20:34 -0600

    adding ficha number

--- public/js/controllers/BoardPageCtrl.js
+++ public/js/controllers/BoardPageCtrl.js
@@ -3,7 +3,10 @@ angular.module ('bankApp.controllers')
 .controller('BoardPageCtrl', ['$scope', 
 	function ($scope) {
 	    $scope.getButtonData = function(obj) {
-	        alert(obj.target.attributes.data.value);
+	        // alert(obj.target.attributes.data.value);
 	    }
+	    $scope.advstatus = true;
+	    
 	}
+
 ])
